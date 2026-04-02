99db82803acc22a586bd95c7aec52c721c9e51c6
dcchill <dariancsmart@gmail.com>
2026-04-02 11:04:05 -0400

    MCreator Merge commit 'a2654818a02c32555f9207d386c445a64815426c' into 2d15b1655a2bc2fcb0694855bcc0e10d0ab6502d

--- src/main/java/net/createteleporters/CreateteleportersMod.java
+++ src/main/java/net/createteleporters/CreateteleportersMod.java
@@ -11,7 +11,6 @@ import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.capabilities.EntityCapability;
 import net.neoforged.fml.util.thread.SidedThreadGroups;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModList;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -27,7 +26,6 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.core.registries.BuiltInRegistries;
 
 import net.createteleporters.network.CreateteleportersModVariables;
-import net.createteleporters.integration.CreateTrainPortalIntegration;
 import net.createteleporters.init.CreateteleportersModTabs;
 import net.createteleporters.init.CreateteleportersModMenus;
 import net.createteleporters.init.CreateteleportersModItems;
@@ -50,33 +48,27 @@ public class CreateteleportersMod {
 
 	public CreateteleportersMod(IEventBus modEventBus) {
 		// Start of user code block mod constructor
-		NeoForge.EVENT_BUS.register(this);
-		modEventBus.addListener(this::registerNetworking);
-		modEventBus.addListener(this::commonSetup);
 		// End of user code block mod constructor
 		NeoForge.EVENT_BUS.register(this);
 		modEventBus.addListener(this::registerNetworking);
+
 		CreateteleportersModBlocks.REGISTRY.register(modEventBus);
 		CreateteleportersModBlockEntities.REGISTRY.register(modEventBus);
 		CreateteleportersModItems.REGISTRY.register(modEventBus);
+
 		CreateteleportersModTabs.REGISTRY.register(modEventBus);
 		CreateteleportersModVariables.ATTACHMENT_TYPES.register(modEventBus);
+
 		CreateteleportersModMenus.REGISTRY.register(modEventBus);
+
 		CreateteleportersModFluids.REGISTRY.register(modEventBus);
 		CreateteleportersModFluidTypes.REGISTRY.register(modEventBus);
+
 		// Start of user code block mod init
 		// End of user code block mod init
 	}
 
 	// Start of user code block mod methods
-	private void commonSetup(final FMLCommonSetupEvent event) {
-		event.enqueueWork(() -> {
-			if (ModList.get().isLoaded("create")) {
-				CreateTrainPortalIntegration.register();
-			}
-		});
-	}
-
 	// End of user code block mod methods
 	private static boolean networkingRegistered = false;
 	private static final Map<CustomPacketPayload.Type<?>, NetworkMessage<?>> MESSAGES = new HashMap<>();
@@ -92,12 +84,9 @@ public class CreateteleportersMod {
 
 	@SuppressWarnings({"rawtypes", "unchecked"})
 	private void registerNetworking(final RegisterPayloadHandlersEvent event) {
-		if (networkingRegistered)
-			return;
-		networkingRegistered = true;
-
 		final PayloadRegistrar registrar = event.registrar(MODID);
 		MESSAGES.forEach((id, networkMessage) -> registrar.playBidirectional(id, ((NetworkMessage) networkMessage).reader(), ((NetworkMessage) networkMessage).handler()));
+		networkingRegistered = true;
 	}
 
 	private static final Collection<Tuple<Runnable, Integer>> workQueue = new ConcurrentLinkedQueue<>();
